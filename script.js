9cc7dd1ce05e8d91e77af0c4fe8b9c36013e85d0
Amina <87755052+mimalson@users.noreply.github.com>
2022-07-01 09:24:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-var name = 'Muhammed Amina'
-var height = '5.7ft tall'
-var country = 'Nigeria'
+const name = 'Muhammed Amina'
+const height = '5.7ft tall'
+const country = 'Nigeria'
 
 alert('My name is' name + "\n" + 'I'm' height + "\n"+ country);
