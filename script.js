0ce8c7b2ae04f951336761471e5c0c3e7a9b7150
Amina <87755052+mimalson@users.noreply.github.com>
2022-07-02 15:18:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const name = 'My name is Muhammed Amina'
 const height = "I'm 5.7ft tall"
 const country = "I'm from Nigeria"
 
-document.write(name "<br>")
+document.write(name)
 document.write(height)
 document.write(country)
